c532a22a927a889e149276079891b90b4e7e5131
Tonny <musembitonnyj@gmail.com>
2022-01-26 15:58:03 +0300

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,22 @@ module.exports = {
                 "regal-blue": "#243c5a",
             },
         },
-
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            'white': '#ffffff',
+            'purple': '#3f3cbb',
+            'midnight': '#121063',
+            'metal': '#565584',
+            'tahiti': '#3ab7bf',
+            'silver': '#ecebff',
+            'bubble-gum': '#ff77e9',
+            'bermuda': '#78dcca',
+        },
     },
     variants: {
         extend: {},
