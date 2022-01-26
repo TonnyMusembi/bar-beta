063ec23768a3834aed69bd219f80c086392e0eaf
Tonny <musembitonnyj@gmail.com>
2022-01-26 15:37:23 +0300

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,16 @@ module.exports = {
     purge: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
-        colors: {
-            "regal-blue": "#243c5a",
+        extend: {
+            backgroundImage: {
+                'hero-pattern': "url('/img/hero-pattern.svg')",
+                'footer-texture': "url('/img/footer-texture.png')",
+            },
+            colors: {
+                "regal-blue": "#243c5a",
+            },
         },
+
     },
     variants: {
         extend: {},
