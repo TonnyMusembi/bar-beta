13975c6cb461a8df12465c80b27bd46b15ae5c80
Tonny <musembitonnyj@gmail.com>
2022-01-31 11:18:56 +0300

    add

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,12 @@
   <router-view />
 </template>
 <script>
-// import Header from '@/components/Header'
+//import Sidebar from "./components/Sidebar";
 export default {
   components: {
-    // Header,
+    //Sidebar,
   },
+
   setup() {},
 };
 </script>
